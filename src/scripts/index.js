ba9a75ec5d642c3ea6a2d5049839b6c2e4b717ad
Jacky Chen <chenjacky32@gmail.com>
2024-11-05 17:02:33 +0700

    add: restaurant item card

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../scripts/roots";
+import data from "../public/data/DATA.json";
 
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
@@ -27,3 +28,31 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 3000);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const restaurants = data.restaurants;
+  const restaurantGrid = document.getElementById("restaurantGrid");
+
+  // Iterasi setiap item di data JSON
+  restaurants.forEach((restaurant) => {
+    // Buat elemen card baru
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    // Isi konten card
+    card.innerHTML = `
+      <div class="image-container">
+        <span class="location">Kota. ${restaurant.city}</span>
+        <img src="${restaurant.pictureId}" alt="${restaurant.name}">
+      </div>
+      <div class="card-content">
+        <p class="rating">Rating: ${restaurant.rating}</p>
+        <h2 class="restaurant-name">${restaurant.name}</h2>
+        <p class="description">${restaurant.description}</p>
+      </div>
+      `;
+
+    // Tambahkan card ke grid
+    restaurantGrid.appendChild(card);
+  });
+});
