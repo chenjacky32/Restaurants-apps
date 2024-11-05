f15fd021944ac264ac7cac3f09076f7a3caf268f
Jacky Chen <chenjacky32@gmail.com>
2024-11-05 15:36:37 +0700

    add: hero section and sliding image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,28 @@ import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../scripts/roots";
 
-console.log("Hello Coders! :)");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active", "previous");
+
+    // Atur slide saat ini
+    if (i === index) {
+      slide.classList.add("active");
+    }
+
+    // Atur slide sebelumnya
+    if (i === (index - 1 + slides.length) % slides.length) {
+      slide.classList.add("previous");
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
