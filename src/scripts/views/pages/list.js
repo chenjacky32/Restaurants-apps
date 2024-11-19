1ff2134e1683fb3dbf053e9ee6f95ec607c8430e
Jacky Chen <chenjacky32@gmail.com>
2024-11-19 23:30:22 +0700

    refactor:  list pages and liked pages to MVP pattern

--- src/scripts/views/pages/list.js
+++ src/scripts/views/pages/list.js
@@ -1,44 +1,16 @@
-import { createItemTemplate } from '../templates/templates-creator';
-import ImageSlider from '../../utils/image-slider';
 import RestaurantSource from '../../data/restaurant-source';
+import ListRestaurantsView from './list-restaurants/list-restaurants-view';
+import ListRestaurantsPresenter from './list-restaurants/list-restaurants-presenter';
+
+const view = new ListRestaurantsView();
 
 const List = {
   async render() {
-    return `<article>
-        <section class="hero-section" aria-labelledby="hero-title">
-          <div class="hero-group">
-            <div class="container-image">
-              <div class="hero-content">
-                <h1 tabindex="4">Find Your Favorite Restaurant Here!</h1>
-                <p tabindex="5">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta dolorem fugit praesentium harum possimus, aliquam explicabo ex veniam architecto illo officia nostrum ipsa aspernatur eligendi aliquid obcaecati deleniti,
-                  omnis provident!
-                </p>
-              </div>
-              <div class="slide slide1"></div>
-              <div class="slide slide2"></div>
-              <div class="slide slide3"></div>
-            </div>
-          </div>
-        </section>
-        <section class="item-section" aria-labelledby="restaurant-title">
-          <h1 id="restaurant-title" tabindex="6">List Restaurant</h1>
-          <div class="restaurant-grid" id="restaurantGrid"></div>
-        </section>
-      </article>`;
+    return view.getTemplate();
   },
+
   async afterRender() {
-    const slides = document.querySelectorAll('.slide');
-    ImageSlider.init({ slides });
-    const restaurantData = await RestaurantSource.listRestaurants();
-    const restaurantGrid = document.querySelector('#restaurantGrid');
-    restaurantData.forEach((restaurant, index) => {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.setAttribute('tabindex', `${7 + index}`);
-      card.innerHTML += createItemTemplate(restaurant);
-      restaurantGrid.appendChild(card);
-    });
+    new ListRestaurantsPresenter({ view, listRestaurants: RestaurantSource });
   },
 };
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
